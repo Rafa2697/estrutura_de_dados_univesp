b133e4998dfeb5aa85946c0a93cf72f0b053c428
Rafael <rafael.mattos468@gmail.com>
2026-08-21 17:01:17 -0300

    treinando com ponteiros

--- semana_5/exercicios/ponteiros.cpp
+++ semana_5/exercicios/ponteiros.cpp
@@ -1,6 +1,20 @@
 using namespace std;
+#include <iostream>
 
 int main() {
 
-    
+    int a = 10;
+    int b = 20;
+
+    cout <<"valor de a: " << a;
+    cout <<"\nvalor de b: " << b;
+
+    cout << "endereco na memoria de a: " << &a << endl;
+    cout << "endereco na memoria de b: " << &b << endl;
+
+    int *ponteiroA = &a;
+    int *enderecoA = &a;
+    cout << " valor de a atraves do ponteiro: " << *ponteiroA << endl;
+    cout << " endereco de a: " << enderecoA << endl;
+
 }
